dff2232cc98f5b86338f0cdd1b751ba769c44474
Shoubhit Dash <shoubhit2005@gmail.com>
2021-12-23 11:40:22 +0530

    added a GIF-fy command
    it translates phrases into GIFs using the GIPHY API

---
+++ cogs/gif.py
@@ -0,0 +1,35 @@
+import discord
+import random
+from discord import colour
+import giphy_client
+from pprint import pprint
+from giphy_client.rest import ApiException
+from discord.ext import commands
+from discord.commands import slash_command, Option
+from settings import GIPHY_API_KEY
+
+url = "https://api.giphy.com/v1/gifs/translate"
+
+class Gif(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @slash_command(guild_ids=[918349390995914792], description="GIF-fy a phrase")
+    async def giffy(self, ctx, phrase: Option(str, "Enter a phrase you want to GIF-fy", required=True, default="wtf")):
+        api_key = GIPHY_API_KEY
+        api_instance = giphy_client.DefaultApi()
+
+        try:
+            api_response = api_instance.gifs_translate_get(api_key, phrase)
+            data = api_response.data
+            embed = discord.Embed(title=phrase, colour=discord.Colour.random())
+            embed.set_image(url=f"https://media.giphy.com/media/{data.id}/giphy.gif")
+            embed.set_footer(icon_url = ctx.author.avatar.url, text = f"Requested by {ctx.author.name} | Powered by GIPHY")
+
+            await ctx.respond(embed=embed)
+        except ApiException as e:
+            await ctx.respond("Something went wrong. Please try again.")
+
+        
+def setup(bot):
+    bot.add_cog(Gif(bot))
